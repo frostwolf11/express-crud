dba6c185de79143e3e62fe4c28cd602e4e8bc451
aishwary <kaulaishwary11@gmail.com>
2020-02-21 14:51:05 +0530

    jwt and express validator

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,27 +1,28 @@
-const access_tokenModel = require("../models/access-token");
 const jwt = require('jsonwebtoken')
 const secret = require('../config')
 
 exports.Auth = async (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).json({
+      return res.status(401).json({
       message: "Auth token missing in header"
     });
   }
+  let token = req.headers.authorization.split(" ");
   try{
-   console.log(secret.jwtSecret);
-   let token = req.headers.authorization.split(" ");
-   const checkJwt = jwt.verify(token[1],secret.jwtSecret);
-   console.log("HMMM");
-   
-   req.userDate = checkJwt;
-   console.log("here");
-   
-   next();
+    const checkJwt = await jwt.verify(token[1],secret.jwtSecret);
+    if (Date.now() >= checkJwt.exp *1000){
+      res.status(401).json({
+        message : "Auth token expired"
+      })
+    } 
+    req.userData = checkJwt;
+    console.log(req.userData);
+    
+    next();
+    
   }catch(error){
     return res.status(401).json({
-      "message": 'Auth failed'
-    })
-  }
-  
+      message: "Auth token invalid"
+    });
+  }  
 };
